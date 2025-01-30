b183c81aaf92931f4ec1db7f0ae8608d7ec479f7
moauiz-09 <rlqqyn@gmail.com>
2025-01-31 00:50:45 +0100

    add file js  and foter for bach

---
+++ main.js
@@ -0,0 +1,12 @@
+// hello world! وي  سلعة
+let btn = document.getElementById("icon");
+let ul = document.getElementById("ul");
+let ls = document.querySelector('.a2');
+btn.onclick =function name(params) {
+    ul.classList.toggle('h');
+    ls.classList.toggle('ad')
+}
+console.log(btn);
+console.log(ul);
+console.log(ls);
+
